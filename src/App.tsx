caacc3b774819f53b020d37a38b3b335a0662c04
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-09-29 19:28:16 +0500

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Block from './component/Block';
 
 function App() {
+  const [state, setState]=useState(Array(9).fill('null'));
   return <div className='board'>
     <div className="board">
             <div className="row">
