b69c56efde18d9c920ec6875d168e6cb6525507d
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-09-30 03:29:00 +0500

    winner logic started

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,20 @@ import Block from './component/Block';
 function App() {
   const [state, setState]=useState(Array(9).fill('null'));
   const [current_turn, set_current_turn]=useState("X");
+  const checkWinner=()=>{
+    const win=[
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ];
+    for(let i = 0; i <window.length; i++) {
+      const [a,b,c]=win[i];
+  };
   const handleBlockClick = (index: number) => {
     const stateCopy=Array.from(state);
     stateCopy[index] = current_turn;
