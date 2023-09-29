b086228a589ef6270c20a9e3525f9c829cab3734
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-09-30 03:22:54 +0500

    states implemented

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,29 @@ import Block from './component/Block';
 function App() {
   const [state, setState]=useState(Array(9).fill('null'));
   const [current_turn, set_current_turn]=useState("X");
+  const handleBlockClick = (index: number) => {
+    const stateCopy=Array.from(state);
+    stateCopy[index] = current_turn;
+    set_current_turn(current_turn==='X' ? 'O' : 'X');
+    setState(stateCopy);
+  };
+
   return <div className='board'>
     <div className="board">
             <div className="row">
-                <Block value={state[0]}/>
-                <Block value={state[1]}/>
-                <Block value={state[2]}/>
+                <Block onclick={()=>handleBlockClick(0)} value={state[0]}/>
+                <Block onclick={()=>handleBlockClick(1)} value={state[1]}/>
+                <Block onclick={()=>handleBlockClick(2)} value={state[2]}/>
             </div>
             <div className="row">
-                <Block value={state[3]}/>
-                <Block value={state[4]}/>
-                <Block value={state[5]}/>
+                <Block onclick={()=>handleBlockClick(3)} value={state[3]}/>
+                <Block onclick={()=>handleBlockClick(4)} value={state[4]}/>
+                <Block onclick={()=>handleBlockClick(5)} value={state[5]}/>
             </div>
             <div className="row">
-                <Block value={state[6]}/>
-                <Block value={state[7]}/>
-                <Block value={state[8]}/>
+                <Block onclick={()=>handleBlockClick(6)} value={state[6]}/>
+                <Block onclick={()=>handleBlockClick(7)} value={state[7]}/>
+                <Block onclick={()=>handleBlockClick(8)} value={state[8]}/>
             </div>
 
         </div>
